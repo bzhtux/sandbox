1e76d3da9837d434554ef53089c58e8fa020c572
Yannick Foeillet <bzhtux@gmail.com>
2019-08-20 10:44:57 +0200

    Add task to re-fly

---
+++ tasks/fly/fly.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env sh
+
+# DL fli-cli
+wget -o /tmp/fly "http://10.36.68.29:8080/api/v1/cli?arch=amd64&platform=linux"
+
+# CHMOD +x
+chmod +x /tmp/fly
+
+# LOGIN
+/tmp/fly login -t http://10.36.68.29:8080/ -u "${USERNAME}" -p "${PASWORD}"
+
+# RE FLY       
+/tmp/fly -t local sp -p display-version -c ci/pipelines/version-pipeline.yml --var version="${NEW_VERSION}" --non-interactive
