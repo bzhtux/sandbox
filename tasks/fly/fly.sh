039a3f9cded24f8d89ab884ae6dfe6e1b8f58b2c
Yannick Foeillet <bzhtux@gmail.com>
2019-08-20 16:12:39 +0200

    Go for it #39

--- tasks/fly/fly.sh
+++ tasks/fly/fly.sh
@@ -22,7 +22,7 @@ chmod +x /tmp/fly
 # LOGIN
 /tmp/fly -t local login --concourse-url http://172.20.0.3:8080/ -u "${USERNAME}" -p "${PASSWORD}"
 
-ls -l ${NEW_VERSION}
+NEW_VERSION=$(cat new_version/version)
 
 # RE FLY       
 /tmp/fly -t local sp -p display-version -c ci/pipelines/version-pipeline.yml --var version="${NEW_VERSION}" --non-interactive
