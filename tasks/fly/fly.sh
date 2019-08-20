20ad0d2b39d0efcdc46a4f89a23ca429e13440ad
Yannick Foeillet <bzhtux@gmail.com>
2019-08-20 14:54:03 +0200

    Go for it #24

--- tasks/fly/fly.sh
+++ tasks/fly/fly.sh
@@ -6,17 +6,17 @@ echo "* Hello from fly.sh"
 
 
 # DL fli-cli
-wget -O /tmp/fly.tgz "https://github.com/concourse/concourse/releases/download/v5.4.1/fly-5.4.1-linux-amd64.tgz"
+wget -O /tmp/fly.tgz -q "https://github.com/concourse/concourse/releases/download/v5.4.1/fly-5.4.1-linux-amd64.tgz"
 
 cd /tmp
 
-tar -xzvf fly.tgz
+tar -xzf fly.tgz
 
 # CHMOD +x
 chmod +x /tmp/fly
 
 # LOGIN
-/tmp/fly login -t http://172.20.0.3:8080/ -u "${USERNAME}" -p "${PASSWORD}"
+/tmp/fly login --concourse-url http://172.20.0.3:8080/ -u "${USERNAME}" -p "${PASSWORD}"
 
 # RE FLY       
 /tmp/fly -t local sp -p display-version -c ci/pipelines/version-pipeline.yml --var version="${NEW_VERSION}" --non-interactive
