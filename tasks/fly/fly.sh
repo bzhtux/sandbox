04b6741c71be03d6b0c76a15642b296b55b914b2
Yannick Foeillet <bzhtux@gmail.com>
2019-08-21 21:54:13 +0200

    try pipe to redirect version to number file

--- tasks/fly/fly.sh
+++ tasks/fly/fly.sh
@@ -13,7 +13,7 @@ tar -xzf /tmp/fly.tgz -C /tmp
 # cd $OLDPWD
 
 /bin/ls -ltr new_version/
-cat new_version/version
+cat new_version/number
 
 # CHMOD +x
 chmod +x /tmp/fly
@@ -21,7 +21,7 @@ chmod +x /tmp/fly
 # LOGIN
 /tmp/fly -t local login --concourse-url http://172.20.0.3:8080/ -u "${USERNAME}" -p "${PASSWORD}"
 
-NEW_VERSION=$(cat new_version/version)
+NEW_VERSION=$(cat new_version/number)
 
 # RE FLY       
 /tmp/fly -t local sp -p display-version -c ci/pipelines/version-pipeline.yml --var version="${NEW_VERSION}" --non-interactive
