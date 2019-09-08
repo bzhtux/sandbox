3cd2e562198474e90d2e347fdb52de62cb37c5a2
Yannick Foeillet <bzhtux@gmail.com>
2019-09-08 22:45:43 +0200

    Add priority for firewall

--- platform/concourse/terraform/gcp/firewall.tf
+++ platform/concourse/terraform/gcp/firewall.tf
@@ -2,6 +2,7 @@ resource "google_compute_firewall" "jbx-ssh" {
   name          = "jbx-ssh"
   network       = "${google_compute_network.concourse.name}"
   target_tags   = ["jbx-ssh"]
+  priority      = 900
 
 allow {
     protocol    = "icmp"
