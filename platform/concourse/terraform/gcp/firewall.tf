6e406f92f018df04a0af509d6bce46dc8759ac65
Yannick Foeillet <bzhtux@gmail.com>
2019-09-08 22:48:08 +0200

    Fix firewall.tf format

--- platform/concourse/terraform/gcp/firewall.tf
+++ platform/concourse/terraform/gcp/firewall.tf
@@ -5,14 +5,14 @@ resource "google_compute_firewall" "jbx-ssh" {
   priority      = 900
 
 allow {
-    protocol    = "icmp"
+  protocol    = "icmp"
 }
 
-  allow {
-    protocol    = "tcp"
-    ports       = ["22"]
-  }
+allow {
+  protocol    = "tcp"
+  ports       = ["22"]
+}
 
-  source_ranges = ["0.0.0.0/0"]
+source_ranges = ["0.0.0.0/0"]
 }
 
