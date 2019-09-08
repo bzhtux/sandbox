437080dc339a71d62a0bb67e30fab74bd3dfea26
Yannick Foeillet <bzhtux@gmail.com>
2019-09-08 23:08:52 +0200

    Remove nat-ip

--- platform/concourse/terraform/gcp/jumpbox.tf
+++ platform/concourse/terraform/gcp/jumpbox.tf
@@ -16,7 +16,7 @@ resource "google_compute_instance" "jumpbox" {
   network_interface {
     subnetwork   = "${var.jbx_subnet}"
     access_config {
-      nat_ip = "${google_compute_address.jumpbox-ip.address}"
+    #   nat_ip = "${google_compute_address.jumpbox-ip.address}"
     }
   }
 
