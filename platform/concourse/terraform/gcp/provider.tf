ac7b83e5fe513fcd4d3a6b8266d3a0446d65e79b
Yannick Foeillet <bzhtux@gmail.com>
2019-09-09 21:51:23 +0200

    Fix gcp credentials

--- platform/concourse/terraform/gcp/provider.tf
+++ platform/concourse/terraform/gcp/provider.tf
@@ -4,9 +4,14 @@ provider "google" {
   region      = "${var.region}"
   zone        = "${var.region}-c"
 
-  version = ">= 1.7.0"
+  # version = ">= 1.7.0"
 }
 
 terraform {
-  required_version = ">= 0.12.0"
+  # required_version = ">= 0.12.0"
+  backend "gcs" {
+  bucket  = "bzhtux-tf-state"
+  prefix  = "concourse/gcp/terraform.tfstate"
+  credentials = "/Users/yfoeillet/workdir/creds/gcp/terraform/cso-pcfs-emea-bzhtux-terraform-service-account.json"
+  }
 }
