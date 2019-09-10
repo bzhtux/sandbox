96d452a1315c70d1211ac3c428d4229691efa53a
Yannick Foeillet <bzhtux@gmail.com>
2019-09-10 22:21:57 +0200

    Update bosh with scp and ssh cmd

--- platform/concourse/ci/bosh.sh
+++ platform/concourse/ci/bosh.sh
@@ -1,7 +1,26 @@
 #!/usr/bin/env bash
 
+WORKDIR=$PWD
+TMP_DIR=$(mktemp -d /tmp/bosh.XXXXXX)
+
 set -x
 
+tearDown(){
+    if [ -d "${TMP_DIR}" ]
+    then
+      rm -rf "${TMP_DIR}"
+    fi
+}
 
-ls -lR terraform/
-cat terraform/metadata
+trap tearDown EXIT
+
+ls -lR "${WORKDIR}"/terraform/
+BOSH_CIDR=$(jq -r .bosh_cidr <"${WORKDIR}"/terraform/metadata)
+BOSH_GW=$(jq -r .bosh_gw <"${WORKDIR}"/terraform/metadata)
+DNS=$(jq -r .dns <"${WORKDIR}"/terraform/metadata)
+
+echo "${SSH_PRIV_KEY}" > "${TMP_DIR}"/ssh_priv_key
+
+scp "${WORKDIR}"/bosh/scripts/deploy.sh -i "${TMP_DIR}"/ssh_priv_key "${SSH_USERNAME}"@jbx."${DNS}": 
+
+ssh -i "${TMP_DIR}"/ssh_priv_key "${SSH_USERNAME}"@jbx."${DNS}" "\"${BOSH_CIDR} ${BOSH_GW}\""
--- platform/concourse/terraform/gcp/output.tf
+++ platform/concourse/terraform/gcp/output.tf
@@ -4,3 +4,7 @@ output "bosh_cidr" {
 output "bosh_gw" {
   value = "${google_compute_subnetwork.bosh.gateway_address}"
 }
+
+output "dns" {
+  value = "${var.dns_zone}.${var.dns_name}"
+}
