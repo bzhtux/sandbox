8e859b5acb7f268ef748e4382a680ae21698a4a2
Yannick Foeillet <bzhtux@gmail.com>
2019-09-12 23:42:26 +0200

    Fix ssh cmd with k=v cmd

--- platform/concourse/ci/bosh.sh
+++ platform/concourse/ci/bosh.sh
@@ -20,10 +20,11 @@ chmod 0400 "${TMP_DIR}"/ssh_priv_key
 # scp -i "${TMP_DIR}"/ssh_priv_key -vvv -o StrictHostKeyChecking=no "${WORKDIR}"/terraform/metadata "${SSH_USERNAME}"@jbx."${DNS%.}":~/tf_metadata 
 DNS=$(jq -r .dns <"${WORKDIR}"/terraform/metadata)
 
-cat >"${TMP_DIR}"/env_vars <<EOF
-export BOSH_CIDR=$(jq -r .bosh_cidr <"${WORKDIR}"/terraform/metadata)
-export BOSH_GW=$(jq -r .bosh_gw <"${WORKDIR}"/terraform/metadata)
-EOF
-
 # shellcheck disable=SC2002
-cat "${TMP_DIR}"/env_vars | ssh -i "${TMP_DIR}"/ssh_priv_key -vvv -o StrictHostKeyChecking=no -o UpdateHostKeys=no "${SSH_USERNAME}"@jbx."${DNS%.}" "/home/${SSH_USERNAME}/deploy.sh ${BOSH_CIDR} ${BOSH_GW}"
+ssh -i "${TMP_DIR}"/ssh_priv_key -vvv \
+-o StrictHostKeyChecking=no \
+-o UpdateHostKeys=no \
+"${SSH_USERNAME}"@jbx."${DNS%.}" \
+"BOSH_CIDR=$(jq -r .bosh_cidr <"${WORKDIR}"/terraform/metadata) \
+BOSH_GW=$(jq -r .bosh_gw <"${WORKDIR}"/terraform/metadata)\
+/home/${SSH_USERNAME}/deploy.sh"
