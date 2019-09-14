6e58afc0a31ff812fb006f20234b09f43015daab
Yannick Foeillet <bzhtux@gmail.com>
2019-09-14 13:29:18 +0200

    Update bosh task with missing variables but not all :-(

--- platform/bosh/ci/task.sh
+++ platform/bosh/ci/task.sh
@@ -2,16 +2,21 @@
 
 set -x
 
+# VARIABLES
 WORKDIR=$PWD
 TMP_DIR=$(mktemp -d /tmp/bosh.XXXXXX)
 BOSH_GIT_URL="https://github.com/cloudfoundry/bosh-deployment.git"
 BOSH_CIDR=$(jq -r .bosh_cidr <"${WORKDIR}"/terraform/metadata)
 BOSH_GW=$(jq -r .bosh_gw <"${WORKDIR}"/terraform/metadata)
-DNS=$(jq -r .dns <"${WORKDIR}"/terraform/metadata)
 BOSH_IP="${BOSH_GW%.1}.10"
+BOSH_SUBNET=$(jq -r .bosh_subnet <"${WORKDIR}"/terraform/metadata)
+DNS=$(jq -r .dns <"${WORKDIR}"/terraform/metadata)
+CREDS=$(jq -r .gcp_json <"${WORKDIR}"/terraform/metadata)
+NET_NAME=$(jq -r .network_name <"${WORKDIR}"/terraform/metadata)
+PROJECT_ID=$(jq -r .network_name <"${WORKDIR}"/terraform/metadata | jq -r .project_id)
 
 
-
+# tearDown
 tearDown(){
     if [ -d "${TMP_DIR}" ]
     then
@@ -21,14 +26,16 @@ tearDown(){
 
 trap tearDown EXIT
 
+
+# BOSH TUNNELING
 echo "${SSH_PRIV_KEY}" > "${TMP_DIR}"/ssh_priv_key
 chmod 0400 "${TMP_DIR}"/ssh_priv_key
-
-
 export BOSH_ALL_PROXY="ssh+socks5://${SSH_USERNAME}@jbx.${DNS%.}:22?private-key=${TMP_DIR}/ssh_priv_key"
 
+# GIT clone bosh-deployment sources
 git clone "${BOSH_GIT_URL}" "${TMP_DIR}"/bosh
 
+# BOSH create env
 bosh create-env "${TMP_DIR}"/bosh/bosh.yml \
 --state "${PWD}/state.json" \
 --ops-file "${TMP_DIR}/bosh/gcp/cpi.yml" \
@@ -40,3 +47,8 @@ bosh create-env "${TMP_DIR}"/bosh/bosh.yml \
 --var internal_ip="${BOSH_IP}" \
 --var internal_gw="${BOSH_GW}" \
 --var internal_cidr="${BOSH_CIDR}" \
+--var gcp_credentials_json="${CREDS}" \
+--var network="${NET_NAME}" \
+--var project_id="${PROJECT_ID}" \
+--var subnetwork="${BOSH_SUBNET}" \
+
