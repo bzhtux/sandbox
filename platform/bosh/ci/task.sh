855d8dfdb54691da5f72238fdd2e763146812af5
Yannick Foeillet <bzhtux@gmail.com>
2019-09-14 13:18:34 +0200

    Add bosh

---
+++ platform/bosh/ci/task.sh
@@ -0,0 +1,42 @@
+#!/usr/bin/env bash
+
+set -x
+
+WORKDIR=$PWD
+TMP_DIR=$(mktemp -d /tmp/bosh.XXXXXX)
+BOSH_GIT_URL="https://github.com/cloudfoundry/bosh-deployment.git"
+BOSH_CIDR=$(jq -r .bosh_cidr <"${WORKDIR}"/terraform/metadata)
+BOSH_GW=$(jq -r .bosh_gw <"${WORKDIR}"/terraform/metadata)
+DNS=$(jq -r .dns <"${WORKDIR}"/terraform/metadata)
+BOSH_IP="${BOSH_GW%.1}.10"
+
+
+
+tearDown(){
+    if [ -d "${TMP_DIR}" ]
+    then
+      rm -rf "${TMP_DIR}"
+    fi
+}
+
+trap tearDown EXIT
+
+echo "${SSH_PRIV_KEY}" > "${TMP_DIR}"/ssh_priv_key
+chmod 0400 "${TMP_DIR}"/ssh_priv_key
+
+
+export BOSH_ALL_PROXY="ssh+socks5://${SSH_USERNAME}@jbx.${DNS%.}:22?private-key=${TMP_DIR}/ssh_priv_key"
+
+git clone "${BOSH_GIT_URL}" "${TMP_DIR}"/bosh
+
+bosh create-env "${TMP_DIR}"/bosh/bosh.yml \
+--state "${PWD}/state.json" \
+--ops-file "${TMP_DIR}/bosh/gcp/cpi.yml" \
+--ops-file "${TMP_DIR}/bosh/uaa.yml" \
+--ops-file "${TMP_DIR}/bosh/credhub.yml" \
+--ops-file "${TMP_DIR}/bosh/jumpbox-user.yml" \
+--vars-store "${PWD}/creds.yml" \
+--var director_name=bosh \
+--var internal_ip="${BOSH_IP}" \
+--var internal_gw="${BOSH_GW}" \
+--var internal_cidr="${BOSH_CIDR}" \
