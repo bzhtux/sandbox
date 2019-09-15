c0a5aee6a6c55d9832e94821fb1619fca60a7c51
Yannick Foeillet <bzhtux@gmail.com>
2019-09-15 19:55:05 +0200

    Add bosh-agent FW rule

--- platform/concourse/terraform/gcp/firewall.tf
+++ platform/concourse/terraform/gcp/firewall.tf
@@ -1,4 +1,4 @@
-resource "google_compute_firewall" "jbx-ssh" {
+resource "google_compute_firewall" "ssh" {
   name          = "jbx-ssh"
   network       = "${google_compute_network.concourse.name}"
   target_tags   = ["ssh"]
@@ -16,3 +16,20 @@ allow {
 source_ranges = ["0.0.0.0/0"]
 }
 
+resource "google_compute_firewall" "bosh-agent" {
+  name          = "bosh-agent"
+  network       = "${google_compute_network.concourse.name}"
+  target_tags   = ["bosh-agent"]
+  priority      = 900
+
+allow {
+  protocol    = "icmp"
+}
+
+allow {
+  protocol    = "tcp"
+  ports       = ["6868"]
+}
+
+source_ranges = ["0.0.0.0/0"]
+}
